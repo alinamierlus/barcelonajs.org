c1fc3cf6fa1b4185bd3d9a959dcfa3df97d5dfa5
Alina Mierlus <mierlus@gmail.com>
2013-09-09 17:09:25 +0200

    FirefosOS talk - added twitter @alina_mierlus

--- app/scripts/2013.js
+++ app/scripts/2013.js
@@ -128,7 +128,7 @@ var bcnjs_events = [
 			{
 				title: 'FirefoxOS (2) - opening the web to billions of new web developers and users',
 				presenter: {
-					username: 'alinamierlus',
+					username: 'alina_mierlus',
 					displayname: 'Alina Mierlus'
 				},
 				description: 'FirefoxOS is the new project of Mozilla - a global movement to empower and bring choice in the lives of new billions of Internet users. FirefoxOS runs on very cheap hardware, bringing the web to people in development markets and opportunities to new web developers. In this talk we will demo some of the capabilities of FirefoxOS.',
